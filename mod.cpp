88ba97a8457763e494e0a49a9fab55618732b30b
Tom_48_97 ☠ <tom_48_97@me.com>
2013-08-19 01:01:02 +0200

    Update mod.cpp

--- mod.cpp
+++ mod.cpp
@@ -1,4 +1,4 @@
 name = "Cold War Rearmed 2";
-picture = "cwr2_logo.paa";
+picture = "cwr2_logo.paa"; //optional to use different mod picture, mod.paa is default
 actionName = "Website";
-action = "http://cwr2.arma2.fr/";
+action = "http://cwr2.arma2.fr/"; //only websites are currently possible here
